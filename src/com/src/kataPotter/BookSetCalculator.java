152775bbd9627e3b9ea55336565b2bf163f8c0ec
Danil Korotenko <danil.korotenko@gmail.com>
2014-10-23 02:56:55 +0300

    Implemented discount calculation.

--- src/com/src/kataPotter/BookSetCalculator.java
+++ src/com/src/kataPotter/BookSetCalculator.java
@@ -3,10 +3,37 @@ package com.src.kataPotter;
 public class BookSetCalculator 
 {
 	private static final int kBookPrice = 8;
+	private static final double[] kBookDiscount = 
+		{
+			0, 		// 0% for 0 books 
+			1, 		// 0% for 1 book
+			0.95,	// 5% for 2 books
+			0.90,	// 10% for 3 books
+			0.85,	// 15% for 4 books
+			0.75,	// 25% for 5 books
+			0.70,	// 30% for 6 books
+			0.65	// 35% for 7 books
+			};
 	
-	public static float calculatePrice(BookSet bookSet)
+	public static double discount(BookSet bookSet)
 	{
-		float result = bookSet.numberOfBooks() * kBookPrice;
+		double result = 0.0;
+		
+		if (bookSet.numberOfSeries() > kBookDiscount.length)
+		{
+			result = kBookDiscount[kBookDiscount.length];
+		}
+		else 
+		{
+			result = kBookDiscount[bookSet.numberOfSeries()];
+		}
+		return result;
+	}
+	
+	public static double calculatePrice(BookSet bookSet)
+	{
+		double result = bookSet.numberOfBooks() * kBookPrice * 
+				discount(bookSet);
 		return result;
 	}
 }
