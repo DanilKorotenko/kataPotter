627499ce1287d43a37a0312d1212c3a6bd37eb1c
Danil Korotenko <danil.korotenko@gmail.com>
2014-10-23 02:13:34 +0300

    Added simple output.

--- src/com/src/kataPotter/KataPotter.java
+++ src/com/src/kataPotter/KataPotter.java
@@ -1,12 +1,27 @@
 package com.src.kataPotter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KataPotter 
 {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		Map<Integer, Integer> bookMap = new HashMap<Integer, Integer>();
+		bookMap.put(1, 1);
+		bookMap.put(2, 2);
+		bookMap.put(3, 2);
+		bookMap.put(4, 2);
+		bookMap.put(5, 2);
+		bookMap.put(6, 1);
+		
+		BookSet bookSet = new BookSet(bookMap);
+		
 		System.out.println("Hello kata potter.");
+		
+		System.out.println("book series:" + bookSet.numberOfSeries());
+		System.out.println("books quantity:" + bookSet.numberOfBooks());
 	}
 
 }
